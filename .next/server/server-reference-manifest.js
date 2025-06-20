e462c67ebecc2f21898a305e979aece476c3f488
bryanwaine <nwanne.ezeaka@gmail.com>
2025-06-20 10:39:49 +0100

    Update TravelMap

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YY4wCboXdu6odcsvHfxmBwlnww7JR5r+CQ70BMvFWtI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0UNT99TPwrBu+DKkvh7y7BZpLcyDNMswFC80bvEUSvY=\"\n}"
